b7417c19819f2b4b779185f7cc895a1467585f01
tomma <tomma8175@gmail.com>
2022-08-30 22:59:31 +0400

    clean code, no errors

--- security-group.tf
+++ security-group.tf
@@ -1,46 +1,46 @@
 resource "aws_security_group" "public" {
-	name = "${var.key_name}-public-sg"
-	description = "Public internet access"
-	vpc_id = aws_vpc.vpc.id
- 
-	tags   = {
-		Name = "Security Group"
-	}
+  name        = "${var.key_name}-public-sg"
+  description = "Public internet access"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "Security Group"
+  }
 }
 
 resource "aws_security_group_rule" "public-in-http" {
-	type = "ingress"
-	from_port = 80
-	to_port = 80
-	protocol = "tcp"
-	cidr_blocks = ["0.0.0.0/0"]
-	security_group_id = aws_security_group.public.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public.id
 }
 
 resource "aws_security_group_rule" "public-http" {
-	type = "ingress"
-	from_port = 8081
-	to_port = 8081
-	protocol = "tcp"
-	cidr_blocks = ["0.0.0.0/0"]
-	security_group_id = aws_security_group.public.id
+  type              = "ingress"
+  from_port         = 8081
+  to_port           = 8081
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public.id
 }
 
 resource "aws_security_group_rule" "public-in-ssh" {
-	type = "ingress"
-	from_port = 22
-	to_port = 22
-	protocol = "tcp"
-	cidr_blocks = ["0.0.0.0/0"]
-	security_group_id = aws_security_group.public.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public.id
 }
 
 resource "aws_security_group_rule" "public_out" {
-	type = "egress"
-	from_port = 0
-	to_port = 0
-	protocol = "-1"
-	cidr_blocks = ["0.0.0.0/0"]
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 
-	security_group_id = aws_security_group.public.id
+  security_group_id = aws_security_group.public.id
 }
